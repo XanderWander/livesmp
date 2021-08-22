504b781def2f44ef5f0722cd18132559f6b47d60
Lego_freak1999 <6773171+legofreak1999@users.noreply.github.com>
2021-08-22 16:46:54 +0200

    Added Tablist code

--- src/main/kotlin/nl/xanderwander/livesmp/core/Tablist.kt
+++ src/main/kotlin/nl/xanderwander/livesmp/core/Tablist.kt
@@ -6,7 +6,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
-class Tablist: Listener {
+open class Tablist: Listener {
 
     fun updateTab() {
         for (p: Player in Bukkit.getOnlinePlayers()) {
