3cdb153643f6375138eb6388745e99c567fc0fae
XMKeijzer <71379069+XanderDeKeijzer@users.noreply.github.com>
2021-08-27 23:14:05 +0200

    Fixed JoinQuit order

--- src/main/kotlin/nl/xanderwander/livesmp/events/JoinQuit.kt
+++ src/main/kotlin/nl/xanderwander/livesmp/events/JoinQuit.kt
@@ -1,5 +1,6 @@
 package nl.xanderwander.livesmp.events
 
+import nl.xanderwander.livesmp.Main
 import nl.xanderwander.livesmp.modules.StaticModule
 import nl.xanderwander.livesmp.player.PlayerManager
 import nl.xanderwander.livesmp.utils.RunnableHelper
@@ -12,10 +13,10 @@ class JoinQuit: Listener {
 
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) {
+        PlayerManager.addPlayer(event.player)
         StaticModule.updateTab()
         StaticModule.setResourcePack(event.player)
         StaticModule.joinInfo(event.player)
-        PlayerManager.addPlayer(event.player)
 
         event.joinMessage = "§a+ §7${event.player.name}"
     }
@@ -24,6 +25,8 @@ class JoinQuit: Listener {
     fun onQuit(event: PlayerQuitEvent) {
         PlayerManager.remPlayer(event.player)
         RunnableHelper.runLater(1L) { StaticModule.updateTab() }
+        Main.instance.adminMode.forceExit(event.player)
+        StaticModule.updateTab()
 
         event.quitMessage = "§c- §7${event.player.name}"
     }
